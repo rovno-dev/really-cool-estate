f2e6b3feb382180bbf6525cea7c5537339dc06e6
niyazgim <niiaz280228@gmail.com>
2026-09-04 17:22:32 +0300

    update home page

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -6,7 +6,7 @@ import { CopyButton } from "@/components/ui/copy-button";
 
 export default function Home() {
   return (
-    <div className="flex h-screen flex-col items-center justify-center bg-bg text-white px-6 overflow-hidden">
+    <div className="flex sm:h-screen flex-col items-center justify-center bg-bg text-white px-6 overflow-hidden">
       <h1 className="text-display-2 text-center mb-10">
         To get started, edit the{" "}
         <code className="rounded bg-white/10 px-2 py-1 font-mono">page.tsx</code>{" "}
@@ -40,8 +40,18 @@ export default function Home() {
           <a href="https://github.com/unidoka/amorfa" target="_blank" rel="noopener noreferrer">GitHub</a>
         </Button>
       </div>
-      <div className="mt-14">
-        <p className="mt-3 text-(--on-bg-low)">You can use <span className="text-(--on-bg-medium)">repomix</span> and <span className="text-(--on-bg-medium)"><Link className="underline" href={"https://github.com/niyazgim/uniskilla"}>lazy-accurate skill</Link></span> to have agentic-like experience on any chat AI</p>
+      <div className="mt-14 flex flex-col sm:flex-row items-start sm:items-center gap-2">
+        <p className="text-(--on-bg-low)">You can use <span className="text-(--on-bg-medium)">repomix</span> and <span className="text-(--on-bg-medium)"><Link className="underline" href={"https://github.com/niyazgim/uniskilla"}>lazy-accurate skill</Link></span> to have agentic-like experience on any chat AI</p>
+        <p className="text-(--on-bg-low) hidden sm:inline-block">|</p>
+        <a
+          href="https://unidoka.com/amorfa"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Made on Amorfa"
+          className="mt-3 sm:m-0"
+        >
+          <img src="/made-on-amorfa-badge-v0.svg" alt="made-on-amorfa-badge-v0" />
+        </a>
       </div>
     </div>
   );
